5533a7273a1ed2191f04c75b5034ebd439552da1
Jasper125 <jasper@dijksma.org>
2025-10-17 12:22:26 +0200

    feat(UC18-UC19): Database-integratie voor boodschappenlijstitems en producten voltooid
    - ProductRepository volledig omgezet naar SQLite met UNIQUE constraint op Name
    - Add-, Update- en Delete-methoden geïmplementeerd
    - ProductService uitgebreid met Add/Get/Delete
    - NewProductView en ViewModel toegevoegd voor nieuwe productinvoer
    - ToolbarItem toegevoegd in ProductView voor navigatie naar invoerscherm
    - Automatische refresh van productlijst bij terugkeer
    - Database herbouwt automatisch zonder duplicatie

--- Grocery.App/Views/ProductView.xaml.cs
+++ Grocery.App/Views/ProductView.xaml.cs
@@ -1,12 +1,21 @@
 using Grocery.App.ViewModels;
 
-namespace Grocery.App.Views;
-
-public partial class ProductView : ContentPage
+namespace Grocery.App.Views
 {
-	public ProductView(ProductViewModel viewModel)
-	{
-		InitializeComponent();
-		BindingContext = viewModel;
-	}
+    public partial class ProductView : ContentPage
+    {
+        private readonly ProductViewModel _viewModel;
+
+        public ProductView(ProductViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.LoadProducts();
+        }
+    }
 }
